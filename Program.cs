12d1c10ee18fdf149dfc875cf30af5af241be431
rafiqul-rasel <rafiqulcseduet@gmail.com>
2021-04-02 12:57:44 +0600

    initial commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fundamental_of_cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //print Hello World
+            first_program fp = new first_program();
+            fp.HelloWorld();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ first_program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace fundamental_of_cs
+{
+    class first_program
+    {
+        public void HelloWorld()
+        {
+            Console.WriteLine("First Program In C#\n");
+            Console.WriteLine("Hello World\n");
+           
+
+        }
+    }
+}
